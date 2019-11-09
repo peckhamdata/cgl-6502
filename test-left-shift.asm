719ef9c57d92117810901a06f366c28557b460b7
worldofchris <chris@worldofchris.com>
2019-11-09 18:48:59 +0000

    fix formatting

--- test-left-shift.asm
+++ test-left-shift.asm
@@ -8,33 +8,33 @@
         sta $d020
         sta $d021
 
-		// given a row of the screen looks like original
+        // given a row of the screen looks like original
 
-		ldy #$00
-loop:	lda original,y
-		sta screen,y
-		iny
-		cpy #$28
-		bne loop
+        ldy #$00
+loop:   lda original,y
+        sta screen,y
+        iny
+        cpy #$28
+        bne loop
 
-		// when we move it left
+        // when we move it left
 
-		//  then we expect it to look like expected
+        //  then we expect it to look like expected
 
-		ldy #$00
-comp:	lda screen,y
-		cmp expected,y
-		bne fail
-		iny
-		cpy #$28
-		bne comp
-		// border goes red if it fails, green if it passes
-pass:	lda #green
-		jmp report
-fail:	lda #red
-report:	sta $d020
-		rts
-		// *=$4000 "Data"
+        ldy #$00
+comp:   lda screen,y
+        cmp expected,y
+        bne fail
+        iny
+        cpy #$28
+        bne comp
+        // border goes red if it fails, green if it passes
+pass:   lda #green
+        jmp report
+fail:   lda #red
+report: sta $d020
+        rts
+        // *=$4000 "Data"
 original: .text "thequickbrownfoxjumpedoverthelazydogTHEQUICKBROWNFOXJUMPEDOVERTHELAZYDOG12345678"
  
 expected: .text "hequickbrownfoxjumpedoverthelazydogTHEQUICKBROWNFOXJUMPEDOVERTHELAZYDOG12345678 "
