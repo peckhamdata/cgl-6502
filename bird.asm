6971fa804042886f79902a3360d3b66a0b460137
worldofchris <chris@worldofchris.com>
2019-11-12 09:44:01 +0000

    Added double buffer to the demo

--- bird.asm
+++ bird.asm
@@ -5,21 +5,28 @@
 :BasicUpstart2(start)
 
 start:	ldx #$00
-!loop:	jsr screen_left
         lda #<buffer
-        sta buffer_lo
+        sta buffer_active_lo
         lda #>buffer
+        sta buffer_active_hi
         sta buffer_hi
-		jsr screen_buffer
-        lda #<buffer
-        sta region_lo
-        lda #>buffer
-        sta region_hi
+        lda #<buff_2
+        sta buffer_pending_lo
+        lda #>buff_2
+        sta buffer_pending_hi
         lda #$0a
-        sta region_x
+        sta buffer_x
+		sta buffer_active_fill
         lda #$19
-        sta region_y
-        jsr region_left
+        sta buffer_y
+!loop:	
+        lda buffer_active_lo
+        sta buffer_lo
+        lda buffer_active_hi
+        sta buffer_hi
+		jsr screen_left
+		jsr screen_buffer
+        jsr double_buffer
 		inx
 		cpx #$20
 		bne !loop-
@@ -31,8 +38,36 @@ start:	ldx #$00
 .import source "screen_left.asm"
 .import source "screen_buffer.asm"
 .import source "region_left.asm"
+.import source "double_buffer.asm"
 
 buffer: .text "abcdefghij"
+        .text "bcdefghijk"
+        .text "cdefghijkl"
+        .text "defghijklm"
+        .text "efghijklmn"
+        .text "fghijklmno"
+        .text "ghijklmnop"
+        .text "hijklmnopq"
+        .text "ijklmnopqr"
+        .text "jklmnopqrs"
+        .text "klmnopqrst"
+        .text "lmnopqrstu"
+        .text "mnopqrstuv"
+        .text "nopqrstuvw"
+        .text "opqrstuvwx"
+        .text "pqrstuvwxy"
+        .text "qrstuvwxyz"
+        .text "rstuvwxyza"
+        .text "stuvwxyzab"
+        .text "tuvwxyzabc"
+        .text "uvwxyzabcd"
+        .text "vwxyzabcde"
+        .text "wxyzabcdef"
+        .text "xyzabcdefg"
+        .text "yzabcdefgh"
+        .text "zabcdefghi"
+
+buff_2: .text "abcdefghij"
         .text "bcdefghijk"
         .text "cdefghijkl"
         .text "defghijklm"
